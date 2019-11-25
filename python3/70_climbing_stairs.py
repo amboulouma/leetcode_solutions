9cacb0da2675d90fe0954c759b4cc800578f0b6b
Amine M. Boulouma <amine@boulouma.com>
2019-11-25 22:54:11 +0100

    Added Mathematicians formula

--- python3/70_climbing_stairs.py
+++ python3/70_climbing_stairs.py
@@ -1,5 +1,13 @@
 import functools
 
+# Recursive approach with memoization
+# O(n) time
+# O(n) space
+class Solution2:
+    @functools.lru_cache()
+    def climbStairs(self, n: int) -> int:
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2) if n > 2 else 2 if n == 2 else 1
+
 # Dynamic approach with space optimization
 # O(n) time
 # O(1) space
@@ -10,10 +18,9 @@ class Solution1:
             a, b = b, a + b
         return a
 
-# Recursive approach with memoization
-# O(n) time
-# O(n) space
-class Solution2:
-    @functools.lru_cache()
+# Mathematician formula
+# O(log(n)) time to calculate the power by squarring
+# O(1) space
+class Solution3:
     def climbStairs(self, n: int) -> int:
-        return self.climbStairs(n - 1) + self.climbStairs(n - 2) if n > 2 else 2 if n == 2 else 1
+        return int((5**.5 / 5) * (((1 + 5**.5)/2)**(n + 1) - ((1 - 5**.5)/2)**(n + 1)))
