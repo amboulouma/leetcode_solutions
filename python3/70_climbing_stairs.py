600aae1d4835648ada0985ed80720f11c2aa02a3
Amine M. Boulouma <amine@boulouma.com>
2019-11-25 22:35:22 +0100

    Added recursive with memoization

--- python3/70_climbing_stairs.py
+++ python3/70_climbing_stairs.py
@@ -1,17 +1,19 @@
+import functools
+
+# Dynamic approach with space optimization
 # O(n) time
 # O(1) space
-# Dynamic approach
-
-class Solution:
+class Solution1:
     def climbStairs(self, n: int) -> int:
-        if n==0: return 0
-        elif n==1: return 1
-        elif n==2: return 2
-        else: 
-            temp_1, temp_2 = 1, 2
-            output = 3
-            for i in range(2, n):
-                output = temp_1 + temp_2
-                temp_1 = temp_2
-                temp_2 = output
-            return output
+        a, b = 1, 1
+        for i in range(n):
+            a, b = b, a + b
+        return a
+
+# Recursive approach with memoization
+# O(n) time
+# O(n) space
+class Solution2:
+    @functools.lru_cache()
+    def climbStairs(self, n: int) -> int:
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2) if n > 2 else 2 if n == 2 else 1
